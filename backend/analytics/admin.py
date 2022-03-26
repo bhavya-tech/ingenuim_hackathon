7a2396105d453ba8fd5cfa833d5ea5b7d361e709
bhavya-tech <bhavya.p@ahduni.edu.in>
2022-03-27 01:44:40 +0530

    added models to admin interface

--- backend/analytics/admin.py
+++ backend/analytics/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Region)
+admin.site.register(Category)
+admin.site.register(Brand)
+admin.site.register(Vendor)
+admin.site.register(ProductType)
+admin.site.register(Inventory)
+admin.site.register(Product)
+admin.site.register(InventoryProduct)
+admin.site.register(Order)
